980f18bfeb196189fdd3ebe39679bc2739a43f13
KTanAug21 <ktan.software.developer@gmail.com>
2024-12-03 12:48:35 +0300

    check signing url issue

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,18 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::get('signed-verify', function () {
+    return redirect(URL::temporarySignedRoute('signed-route', now()->addMinutes(5), ['user' => 1], false));
+});
+
+Route::get('signed-route/{user}', function (Request $request) {
+    if (! URL::hasValidSignature($request, absolute: false)) {
+        abort(401);
+    }
+
+    return response('Valid Signature');
+})->name('signed-route');
 
 require __DIR__.'/auth.php';
