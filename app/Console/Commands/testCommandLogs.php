da9ac5974976784281e7548fbc714b43a27cbcc2
KTanAug21 <ktan.software.developer@gmail.com>
2025-04-03 10:42:30 +0300

    get output

---
+++ app/Console/Commands/testCommandLogs.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+
+class testCommandLogs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test-logs';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+       
+        $showProcess = new Process([
+
+          
+            "composer",
+    
+            "--version"
+    
+        ]);
+    
+        $showProcess->setTimeout(180);
+    
+        $showProcess->mustRun();
+    
+        $output = $showProcess->getOutput();
+        dd($output );
+
+    }
+}
